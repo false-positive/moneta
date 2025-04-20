2efc78ddd2c6d33dccb8b171f323b36f3fd93500
CodenameSource <kaloyanmiladinov@gmail.com>
2025-04-20 21:09:43 +0300

    Added methods to stop,delete a given action at current step

--- web/lib/stores/quest-store.ts
+++ web/lib/stores/quest-store.ts
@@ -7,6 +7,9 @@ import {
 	simulateWithActions,
 } from "@/lib/engine/quests";
 import { questDescriptions } from "../engine/quests/descriptions";
+import { aC } from "vitest/dist/chunks/reporters.d.CqBhtcTq.js";
+import { act } from "react";
+import { steps } from "framer-motion";
 
 const initialQuestDescription = questDescriptions.tutorial;
 
@@ -26,6 +29,32 @@ export const questStore = createStore({
 				quest.steps.length < quest.description.maxStepCount
 					? appendAtEndAndAdvance(quest, event.newActions)
 					: appendAtCurrentStepIndex(quest, event.newActions);
+
+			return {
+				...quest,
+				steps: newSteps,
+				currentStepIndex: newSteps.length - 1,
+			};
+		},
+		currentStepStopAction: (quest, event: { action: Action }) => {
+			const newSteps = stopAction(quest, event.action);
+
+			if (newSteps.length == 0) {
+				return quest;
+			}
+
+			return {
+				...quest,
+				steps: newSteps,
+				currentStepIndex: newSteps.length - 1,
+			};
+		},
+		currentStepDeleteAction: (quest, event: { action: Action }) => {
+			const newSteps = deleteAction(quest, event.action);
+
+			if (newSteps.length == 0) {
+				return quest;
+			}
 
 			return {
 				...quest,
@@ -76,6 +105,63 @@ function appendAtCurrentStepIndex(quest: Quest, newActions: Action[]) {
 		...(newActionsPerStep[quest.currentStepIndex] || []),
 		...newActions,
 	];
+
+	return simulateWithActions(quest.description, newActionsPerStep);
+}
+
+function indexAction(newActionsPerStep: Action[][], action: Action) {
+	var actionStartStep = -1,
+		actionIndex = -1;
+	for (let step = 0; step < newActionsPerStep.length; step++) {
+		for (
+			let iteratedActionIndex = 0;
+			iteratedActionIndex < newActionsPerStep[step].length;
+			iteratedActionIndex++
+		) {
+			if (
+				(action.name =
+					newActionsPerStep[step][iteratedActionIndex].name)
+			) {
+				actionStartStep = step;
+				actionIndex = iteratedActionIndex;
+			}
+		}
+	}
+
+	return [actionStartStep, actionIndex];
+}
+
+function stopAction(quest: Quest, action: Action) {
+	const newActionsPerStep = getNewActionsPerStep(quest);
+
+	const [actionStartStep, actionIndex] = indexAction(
+		newActionsPerStep,
+		action
+	);
+
+	if (actionStartStep != -1 || quest.currentStepIndex > actionStartStep) {
+		newActionsPerStep[actionStartStep][actionIndex].remainingSteps =
+			quest.currentStepIndex - actionStartStep;
+	} else {
+		return [];
+	}
+
+	return simulateWithActions(quest.description, newActionsPerStep);
+}
+
+function deleteAction(quest: Quest, action: Action) {
+	const newActionsPerStep = getNewActionsPerStep(quest);
+
+	const [actionStartStep, actionIndex] = indexAction(
+		newActionsPerStep,
+		action
+	);
+
+	if (actionStartStep != -1 || quest.currentStepIndex > actionStartStep) {
+		newActionsPerStep[actionStartStep].splice(actionIndex, 1);
+	} else {
+		return [];
+	}
 
 	return simulateWithActions(quest.description, newActionsPerStep);
 }
